04796582f4f08642df81763fbbbff18e5528c449
goldcirrus <53105370+goldcirrus@users.noreply.github.com>
2022-11-20 20:22:41 -0800

    Add files via upload

---
+++ src1/Tester.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jcuzzola
+ */
+public class Tester extends Test {
+    
+    public void f()
+    {
+        System.out.println("This is f() in Tester Class which extends Test Class");
+    }
+    
+}
